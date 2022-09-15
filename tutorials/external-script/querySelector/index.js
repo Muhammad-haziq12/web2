94c2a603820c6c28fbd9b1aaf11c8c503b01a7ae
haziq muhamamd <muhammad.haziq@student.vinci.be>
2022-09-15 22:28:55 +0200

    commit 1

---
+++ tutorials/external-script/querySelector/index.js
@@ -0,0 +1,4 @@
+alert("Pop-up thanks to an external JS file.");
+let btn2 = document.querySelector("#myBtn2"); // HTML id attribute
+let msg = document.querySelector(".message"); // CSS class name
+let duplicateMsg = document.querySelector("div"); // HTML tag name
