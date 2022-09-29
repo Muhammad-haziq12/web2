5891de6ce2c71c3959deb0fa0381a49645c39a75
haziq muhamamd <muhammad.haziq@student.vinci.be>
2022-09-29 21:28:55 +0200

    myMovies : module 2 : dynamic homepage

---
+++ exercises/module2/2.2/modern/src/index.js
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/main.css';
+
+
+import Image1 from './img/film.jpg';
+import Image2 from './img/film2.jpg';
+
+const homepage = `
+<div class = "text-center">
+<h1 class="animate__animated animate__bounce text-center">MY MOVIES</h1>
+<p> Here you can find a selection of our favorite movies ; </p>
+
+<div class="pb-3">
+    <img class="img-thumbnail w-50" src="${Image1}" alt="Groot" />
+  </div>
+
+  
+  <div>
+    <img class="img-thumbnail w-50" src="${Image2}" alt="Stormtrooper" />
+  </div>
+</div>`;
+
+const main = document.querySelector('main');
+
+main.innerHTML = homepage;
