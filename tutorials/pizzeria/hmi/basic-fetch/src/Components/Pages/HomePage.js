bdf816001082448baf6c778c04a51e31684fc363
Muhammad-haziq12 <you@example.com>
2022-11-13 14:46:39 +0100

    async-await

--- tutorials/pizzeria/hmi/basic-fetch/src/Components/Pages/HomePage.js
+++ tutorials/pizzeria/hmi/basic-fetch/src/Components/Pages/HomePage.js
@@ -22,13 +22,13 @@ import { clearPage, renderPageTitle } from '../../utils/render';
   const HomePage = () => {
     
     clearPage();
-    fetch('http://localhost:3000/pizzas')
+    fetch('/api/pizzas')
     .then((response) => {
       if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
       return response.json();
     })
-    .then((pizzas) => {
-      renderMenuFromString(pizzas);
+    .then((pizza) => {
+      renderMenuFromString(pizza);
       attachOnMouseEventsToGoGreen();
       renderDrinksFromNodes(DRINKS);
   
